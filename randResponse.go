f71b9226c50d8d7b8a3f0765b379f581ec70bc75
rayman51 <ray.mannionit2@gmail.com>
2017-12-01 19:39:16 +0000

    cleaned up code

--- randResponse.go
+++ randResponse.go
@@ -84,58 +84,7 @@ func Reflect(input string) string {
 	return strings.Join(tokens, " ")
 } // reflect
 func main() {
-	/*var input string // variables used
-	fmt.Println("=======================")
-	fmt.Println("         ELIZA         ")
-	fmt.Println("=======================")
-	fmt.Println("Enter the word :") // user prompt
-	fmt.Scanf("%s\n", &input)
-	// This tests whether a pattern matches a string.
-	fmt.Println(input)
-	fmt.Println(ElizaResponse(input))
-	fmt.Println() // realised i didnt need user input
-	rand.Seed(time.Now().UTC().UnixNano())
 
-	fmt.Println("People say I look like both my mother and father.")
-	fmt.Println(ElizaResponse("People say I look like both my mother and father."))
-	fmt.Println() //matches father
-
-	fmt.Println("Father was a teacher.")
-	fmt.Println(ElizaResponse("Father was a teacher."))
-	fmt.Println() //matches father
-
-	fmt.Println("I was my father’s favourite.")
-	fmt.Println(ElizaResponse("I was my father’s favourite."))
-	fmt.Println() //matches father
-
-	fmt.Println("I’m looking forward to the weekend.")
-	fmt.Println(ElizaResponse("I’m looking forward to the weekend."))
-	fmt.Println()
-
-	fmt.Println("My grandfather was French!")
-	fmt.Println(ElizaResponse("My grandfather was French!"))
-	fmt.Println() //matches father
-
-	fmt.Println("I am happy.")
-	fmt.Println(ElizaResponse("I am happy."))
-	fmt.Println() // matches I am
-
-	fmt.Println("I'm happy.")
-	fmt.Println(ElizaResponse("I'm happy."))
-	fmt.Println() // matches I'm
-
-	fmt.Println("I am not happy with your responses.")
-	fmt.Println(ElizaResponse("I am not happy with your responses."))
-	fmt.Println() // matches I am
-
-	fmt.Println("I am not sure that you understand the effect that your questions are having on me.")
-	fmt.Println(ElizaResponse("I am not sure that you understand the effect that your questions are having on me."))
-	fmt.Println() // matches I am
-
-	fmt.Println("I am supposed to just take what you’re saying at face value?")
-	fmt.Println(ElizaResponse("I am supposed to just take what you’re saying at face value?"))
-	fmt.Println() // matches I am
-	*/
 	for i := 0; i < 14; i++ {
 
 		fmt.Println("Q:" + ques[i])
